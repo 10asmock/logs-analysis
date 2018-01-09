c57594269b58990d5c6aff09881be4406745b75a
10asmock <hegemon1984@gmail.com>
2018-01-09 08:30:58 -0700

    Update newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -2,7 +2,7 @@ import psycopg2
 
 DBNAME = "news"
 
-
+#!/usr/bin/env python3
 def top_articles():
     """Returns the three most viewed articles from 'news' database"""
     db = psycopg2.connect(database=DBNAME)
